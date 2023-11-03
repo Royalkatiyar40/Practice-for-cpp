2fe46163b96287c139d1f35460e7f26b0fbf0933
Ansh Katiyar <108641849+Royalkatiyar40@users.noreply.github.com>
2023-11-03 11:15:59 +0530

    Add files via upload

---
+++ subtraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int a, b,c;
+    cout << "Enter A" << endl;
+    cin>>a;
+    cout << "Enter B" << endl;
+    cin>>b;
+    
+    c=a-b;
+    cout << "Sub:"<<c;
+    return 0;
+}
